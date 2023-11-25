eea82cf52a4d52fbec1c8bbfd4f32aabdd4e2d9c
codeislife07 <codingislife07@gmail.com>
2023-11-25 22:22:11 +0530

    Update screenTitle.dart

--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -7,9 +7,22 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    return TweenAnimationBuilder(
+      tween: Tween<double>(begin: 0,end: 1),
+      duration: Duration(seconds: 1),
+      builder: (BuildContext context, double value, Widget? child) {
+        return Opacity(
+          opacity: value,
+          child: Padding(
+            padding: EdgeInsets.only(top: value*20),
+            child: child!,
+          ),
+        );
+      },
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
