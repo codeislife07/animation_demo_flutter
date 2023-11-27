966caef34a7c20de68307d374671df7b69fd349a
codeislife07 <codingislife07@gmail.com>
2023-11-27 22:35:23 +0530

    Update heart.dart

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,16 +5,69 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+
+  late AnimationController _animationController;
+  late Animation _colorAnimation;
+  late Animation<double> _sizeAnimation;
+
+  bool isFav=false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController=AnimationController(
+        duration: Duration(milliseconds: 300),
+        vsync: this);
+
+    _colorAnimation=ColorTween(begin: Colors.grey,end: Colors.red)
+        .animate(_animationController);
+
+    _sizeAnimation=TweenSequence(
+        <TweenSequenceItem<double>>[
+          TweenSequenceItem(tween: Tween(begin: 30,end: 50),
+              weight: 50),
+          TweenSequenceItem(tween: Tween(begin: 50,end: 30),
+              weight: 50),
+        ]
+    ).animate(_animationController);
+
+    // _animationController.forward();
+
+    _animationController.addListener(() {
+      print(_animationController.value);
+      print(_colorAnimation.value);
+    });
+
+    _animationController.addStatusListener((status) {
+      if(status==AnimationStatus.completed){
+        isFav=true;
+      }else{
+        isFav=false;
+      }
+      setState(() {
+      });
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _colorAnimation,
+      builder: (BuildContext context, Widget? child) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: _sizeAnimation.value,
+          ),
+          onPressed: () {
+            isFav?_animationController.reverse():_animationController.forward();
+          },
+        );
+      },
     );
   }
 }
