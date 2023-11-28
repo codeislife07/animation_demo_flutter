a8dabaaf7ccc505bf695b02df08d6a6749ef0d2c
codeislife07 <codingislife07@gmail.com>
2023-11-28 21:29:26 +0530

    curves

--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -2,14 +2,28 @@ import 'package:flutter/material.dart';
 
 class ScreenTitle extends StatelessWidget {
   final String text;
-
   const ScreenTitle({Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    //in side built in animation
+    return TweenAnimationBuilder(
+      tween: Tween<double>(begin: 0,end: 1),
+      duration: Duration(seconds: 1),
+      curve: Curves.easeIn,
+      builder: (BuildContext context, double value, Widget? child) {
+        return Opacity(
+          opacity: value,
+          child: Padding(
+            padding: EdgeInsets.only(top: value*20),
+            child: child!,
+          ),
+        );
+      },
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
